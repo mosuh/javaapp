f373d5d43a6365e5bcec03cdb3afb1d199c6b9fd
mosuh <mosuh7@gmail.com>
2018-11-21 10:26:50 +0530

    Add files via upload

---
+++ Newapp/src/newmain.java
@@ -0,0 +1,81 @@
+
+//import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+//import javax.swing.border.EmptyBorder;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
+public class newmain extends JFrame {
+
+	
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					newmain frame = new newmain();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public newmain() {
+		setTitle("Calculator and Notepad Program");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 300, 150);
+		contentPane = new JPanel();
+	
+		setContentPane(contentPane);
+		
+		JButton btnNewButton = new JButton("Notepad");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Notepad.main(new String[]{});
+			}
+		});
+	
+	contentPane.add(btnNewButton);
+		
+		JButton button = new JButton("Calculator");
+	button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				MyCalculator.main(new String[]{});
+			}
+		});
+	contentPane.add(button);
+	
+	JButton button2 = new JButton("IP Finder");
+	button2.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent arg0) {
+			IPFinder.main(new String[]{});
+		}
+	});
+	contentPane.add(button2);
+	
+	
+	JButton button3 = new JButton("Puzzle Game");
+	button3.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent arg0) {
+			Puzzle.main(new String[]{});
+		}
+	});
+	contentPane.add(button3);
+
+	contentPane.setLayout(new GridLayout(2,2));
+	}
+}
